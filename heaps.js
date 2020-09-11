9a4a7a64ff193d183afbce6faa57fa49aa014c8b
123JackCole <123jack.cole@gmail.com>
2020-09-11 16:47:24 -0700

    added in swap and moveUp helper methods

--- heaps.js
+++ heaps.js
@@ -1,4 +1,39 @@
-function MaxHeap() {
-    this.heap = [];
+function Heap() {
+    this.content = [];
 }
 
+Heap.prototype = {
+
+    push
+
+}
+
+const swap = (a, b) => {
+    [this.content[a], this.content[b]] = [this.content[b], this.content[a]];
+}
+
+const moveUp = (index) => {
+
+    let element = this.content[index];
+
+    while (index > 0) {
+
+        let parentIndex = Math.floor((index + 1) / 2) - 1, // Math.floor((index + 1) / 2)
+            parent = this.content[parentIndex];
+
+        if (parent >= element) return true;
+
+        swap(parentIndex, index);
+        index = parentIndex;
+
+    }
+
+    return false;
+
+}
+
+const moveDown = (index) => {
+   
+    
+
+}
